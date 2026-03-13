efb6ea89d0d71636558b5f15391d29683632b375
ragediksha <rage.diksha@klh.edu.in>
2026-03-13 12:29:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,257 @@
+/* =========================
+   LOGIN VALIDATION
+========================= */
+
+let form = document.querySelector("form");
+
+if(form){
+form.addEventListener("submit", function(event){
+
+let email = document.querySelector("input[type='email']");
+let password = document.querySelector("input[type='password']");
+
+if(email && password){
+
+if(email.value === "" || password.value === ""){
+alert("Please enter email and password");
+event.preventDefault();
+}
+else{
+alert("Login Successful! Welcome to Kuchipudi");
+}
+
+}
+
+});
+}
+
+
+/* =========================
+   JOIN FORM SUCCESS MESSAGE
+========================= */
+
+function showMessage(event){
+event.preventDefault();
+
+let msg = document.getElementById("successMessage");
+
+if(msg){
+msg.style.display = "block";
+}
+}
+
+
+/* =========================
+   MUDRA PRACTICE TRACKER
+========================= */
+
+let asamyuta = Number(localStorage.getItem("asamyuta")) || 0;
+let samyuta = Number(localStorage.getItem("samyuta")) || 0;
+
+function addAsamyuta(){
+
+if(asamyuta < 28){
+asamyuta++;
+localStorage.setItem("asamyuta", asamyuta);
+updateDisplay();
+}
+else{
+alert("You have completed all 28 Asamyuta Mudras!");
+}
+
+}
+
+function resetAsamyuta(){
+asamyuta = 0;
+localStorage.setItem("asamyuta", asamyuta);
+updateDisplay();
+}
+
+function addSamyuta(){
+
+if(samyuta < 24){
+samyuta++;
+localStorage.setItem("samyuta", samyuta);
+updateDisplay();
+}
+else{
+alert("You have completed all 24 Samyuta Mudras!");
+}
+
+}
+
+function resetSamyuta(){
+samyuta = 0;
+localStorage.setItem("samyuta", samyuta);
+updateDisplay();
+}
+
+function updateDisplay(){
+
+let aCount = document.getElementById("asamyutaCount");
+let sCount = document.getElementById("samyutaCount");
+
+if(aCount && sCount){
+
+aCount.innerText = asamyuta;
+sCount.innerText = samyuta;
+
+let aProgress = (asamyuta/28)*100;
+let sProgress = (samyuta/24)*100;
+
+document.getElementById("asamyutaBar").style.width = aProgress + "%";
+document.getElementById("samyutaBar").style.width = sProgress + "%";
+
+}
+
+}
+
+/* Load values when page opens */
+
+window.onload = updateDisplay;
+
+// Dark Mode Toggle
+
+const toggleBtn = document.getElementById("themeToggle");
+
+toggleBtn.onclick = function(){
+
+document.body.classList.toggle("dark-mode");
+
+if(document.body.classList.contains("dark-mode")){
+toggleBtn.innerHTML = "☀";
+}
+else{
+toggleBtn.innerHTML = "🌙";
+}
+
+};
+
+/* Load saved values */
+
+let asamyuta = localStorage.getItem("asamyuta") || 0;
+let samyuta = localStorage.getItem("samyuta") || 0;
+
+asamyuta = Number(asamyuta);
+samyuta = Number(samyuta);
+
+updateDisplay();
+
+/* Asamyuta counter */
+
+function addAsamyuta(){
+
+if(asamyuta < 28){
+asamyuta++;
+localStorage.setItem("asamyuta", asamyuta);
+updateDisplay();
+}
+else{
+alert("You have completed all 28 Asamyuta Mudras!");
+}
+
+}
+
+function resetAsamyuta(){
+
+asamyuta = 0;
+localStorage.setItem("asamyuta", asamyuta);
+updateDisplay();
+
+}
+
+/* Samyuta counter */
+
+function addSamyuta(){
+
+if(samyuta < 24){
+samyuta++;
+localStorage.setItem("samyuta", samyuta);
+updateDisplay();
+}
+else{
+alert("You have completed all 24 Samyuta Mudras!");
+}
+
+}
+
+function resetSamyuta(){
+
+samyuta = 0;
+localStorage.setItem("samyuta", samyuta);
+updateDisplay();
+
+}
+
+/* Load saved values */
+
+let asamyuta = localStorage.getItem("asamyuta") || 0;
+let samyuta = localStorage.getItem("samyuta") || 0;
+
+asamyuta = Number(asamyuta);
+samyuta = Number(samyuta);
+
+updateDisplay();
+
+/* Asamyuta counter */
+
+function addAsamyuta(){
+
+if(asamyuta < 28){
+asamyuta++;
+localStorage.setItem("asamyuta", asamyuta);
+updateDisplay();
+}
+else{
+alert("You have completed all 28 Asamyuta Mudras!");
+}
+
+}
+
+function resetAsamyuta(){
+
+asamyuta = 0;
+localStorage.setItem("asamyuta", asamyuta);
+updateDisplay();
+
+}
+
+/* Samyuta counter */
+
+function addSamyuta(){
+
+if(samyuta < 24){
+samyuta++;
+localStorage.setItem("samyuta", samyuta);
+updateDisplay();
+}
+else{
+alert("You have completed all 24 Samyuta Mudras!");
+}
+
+}
+
+function resetSamyuta(){
+
+samyuta = 0;
+localStorage.setItem("samyuta", samyuta);
+updateDisplay();
+
+}
+
+/* Update display */
+
+function updateDisplay(){
+
+document.getElementById("asamyutaCount").innerText = asamyuta;
+document.getElementById("samyutaCount").innerText = samyuta;
+
+let aProgress = (asamyuta/28)*100;
+let sProgress = (samyuta/24)*100;
+
+document.getElementById("asamyutaBar").style.width = aProgress + "%";
+document.getElementById("samyutaBar").style.width = sProgress + "%";
+
+}
+
+
